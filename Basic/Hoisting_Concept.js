ad3c0b3b05bec16e6d90e0550acc16fc7b03c437
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-14 22:25:52 +0530

    Hoisting on var keyword

--- Basic/Hoisting_Concept.js
+++ Basic/Hoisting_Concept.js
@@ -1,7 +1,22 @@
-console.log(10+70);
+//console.log(10+70);
 
-greeting()
+//greeting()
 function greeting ()
 {
     console.log("Human is only being to destroy nature")
 }
+
+//keyword hoisting
+// console.log(a);
+// a = 10;
+
+
+var x;
+console.log(x === undefined); // true
+x = 3;
+
+(function () {
+  var x;
+  //console.log(x); // undefined
+  x = "local value";
+})();
