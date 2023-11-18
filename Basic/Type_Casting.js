81ab30659fbe5277267ba2604688d687d7618faa
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-18 12:43:04 +0530

    Object into String

--- Basic/Type_Casting.js
+++ Basic/Type_Casting.js
@@ -1,7 +1,26 @@
-let value = 41;
+/*let value = 41;
 console.log(value);
 console.log(typeof value);
 let typeCastInteger = String(value);
 console.log(typeCastInteger);
 console.log(typeof typeCastInteger);
-console.log(`conversion integer ${value} into the String ${typeCastInteger}`);
+console.log(`conversion integer ${value} into the String ${typeCastInteger}`);*/
+
+
+var  Todate = new Date();
+console.log(Todate);
+console.log(typeof Todate);
+
+//var intoString = String(Todate);
+// console.log(intoString);
+// console.log(typeof intoString);
+
+var array = [1, 2, 3, 4 , 5 ]
+console.log(array)
+console.log(array.length)
+console.log(typeof array)
+
+var intoString = String(array);
+console.log(intoString);
+console.log(typeof intoString);
+console.log(intoString.length) // it convert into char so the length is 9
