a7dc990130aabb5f144d2e72cbdc002f87a9b5da
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-11-18 12:20:11 +0530

    Type Cast

---
+++ Basic/Type_Casting.js
@@ -0,0 +1,7 @@
+let value = 41;
+console.log(value);
+console.log(typeof value);
+let typeCastInteger = String(value);
+console.log(typeCastInteger);
+console.log(typeof typeCastInteger);
+console.log(`conversion integer ${value} into the String ${typeCastInteger}`);
