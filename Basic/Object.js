b1df1ba1ddbee9d89d9a991b835b4a10950ae503
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-21 19:40:59 +0530

    Object some method

--- Basic/Object.js
+++ Basic/Object.js
@@ -20,7 +20,7 @@ userJS.caption = function() {
 }
 console.log(userJS.myFunction());
 console.log(userJS.caption());*/
-
+/*
 const tinder = new Object();
 tinder.id = "14578";
 tinder.name = "Jakson";
@@ -40,5 +40,51 @@ const newUser = {
   },
 };
 console.log(newUser);
-console.log(typeof newUser.fullName);
-console.log( newUser.fullName);
+// console.log(typeof newUser.fullName);
+// console.log( newUser.fullName);
+console.log(tinder)
+
+const combination = Object.assign({}, tinder, newUser)
+console.log(combination)
+*/
+
+const person = {
+    isHuman: false,
+    printIntroduction: function () {
+      console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+    },
+  };
+  
+  const me = Object.create(person);
+  
+  me.name = 'Matthew'; // "name" is a property set on "me", but not on "person"
+  me.isHuman = true; // Inherited properties can be overwritten
+  
+  me.printIntroduction();
+  // Expected output: "My name is Matthew. Am I human? true"
+
+  const object1 = {};
+
+Object.defineProperties(object1, {
+  property1: {
+    value: 42,
+    writable: true,
+  },
+  property2: {},
+});
+
+console.log(object1.property1);
+// Expected output: 42
+
+const object2 = {
+    a: 'somestring',
+    b: 42,
+  };
+  
+  for (const [key, value] of Object.entries(object2)) {
+    console.log(`${key}: ${value}`);
+  }
+  
+  // Expected output:
+  // "a: somestring"
+  // "b: 42"
