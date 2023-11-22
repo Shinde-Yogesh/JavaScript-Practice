c58d0eb2368f6c3a7887abc88118e43b87589bd3
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-22 12:09:55 +0530

    spread operator callobject properties using method

--- Basic/functions_And_Parameters.js
+++ Basic/functions_And_Parameters.js
@@ -13,7 +13,7 @@ function loginUserName(userName) {
   return `${userName} is logged in`;
 }
 console.log(loginUserName("Hitesh"));
-console.log(loginUserName(""));*/
+console.log(loginUserName(""));
 
 //default value for user
 function loginUserName(userName = "Jackson") {
@@ -25,3 +25,34 @@ function loginUserName(userName = "Jackson") {
 }
 // console.log(loginUserName("Hitesh"))
 console.log(loginUserName("Rahul"));
+*/
+/*
+cart shoping example
+Spread operator
+function calculateCartPrice(number)
+{
+    return number;
+}
+console.log(calculateCartPrice(10, 20, 30))
+
+function calculateCartPrice(val1, val2,...number)
+{
+    return number;
+}
+console.log(calculateCartPrice(10, 20, 40))*/
+
+//object with function
+
+const book = {
+  name: "The Untold Story",
+  price: 6487,
+};
+console.log(book);
+
+function callObjectProperties(anyObject) {
+  console.log(
+    `Book Name : ${anyObject.name}  and the price is ${anyObject.price}`
+  );
+}
+
+callObjectProperties(book);
