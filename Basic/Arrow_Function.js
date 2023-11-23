11d8ed612208958a4c815d4ad16c158cd08a7f2b
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-23 21:27:24 +0530

    Arrow function

--- Basic/Arrow_Function.js
+++ Basic/Arrow_Function.js
@@ -1,4 +1,4 @@
-const user = {
+/*const user = {
   username: "hitesh",
   cousrePrice: 4578,
 
@@ -9,8 +9,39 @@ const user = {
 };
 // console.log(user)
 user.message();
-
 user.username = "Jakson";
+user.message();*/
+/*
+function chai()
+{
+    let name = "Jackson";
+    console.log(this.name)      // this function alway work on object
+}
+chai()*/
 
-user.message();
+// const value = function ()
+// {
+//     console.log("work is on the way")
+// }
+// value();
 
+// Here is work with Arrow
+// const value = () =>
+// {
+//    let username = "Jam"
+//     console.log(`${username}, work is on the way`)
+//     console.log(this.username)
+// }
+
+// value();
+
+// syntax for arrow function   () => {}
+// name to the arrow function
+ const value = (num1, num3) => {
+return num1 + num3;
+}
+// console.log(value(1,4))
+// syntax for arrow explicit function   const value = (num1, num3) => num1 + num3;
+
+const sayDigit = () => ({digit : 10}) //here when return the object then () is requered
+console.log(sayDigit())
