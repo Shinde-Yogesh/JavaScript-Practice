b36eca4064bffc26d01299cb2c88e7c15007281b
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-08 17:32:34 +0530

    parent child relationship

--- DOM/One.js
+++ DOM/One.js
@@ -1,34 +1,33 @@
-// document.getElementById("title")
+// // document.getElementById("title")
 
-//code generate the purple of All Li 
-console.log("hello");
-document.querySelectorAll('li')
-const allLi = document.querySelectorAll('li')
-allLi.forEach(function (l) {
-    l.style.backgroundColor = 'purple'
-})
+// //code generate the purple of All Li 
+// console.log("hello");
+// document.querySelectorAll('li')
+// const allLi = document.querySelectorAll('li')
+// allLi.forEach(function (l) {
+//     l.style.backgroundColor = 'purple'
+// })
 
-// get the all files of the class item
-//because class doesn't have loop so convert into the array
-Array.from(allLi)
-newArray.forEach(function (li){
-    li.style.color = 'orange'
-})
+// // get the all files of the class item
+// //because class doesn't have loop so convert into the array
+// Array.from(allLi)
+// newArray.forEach(function (li){
+//     li.style.color = 'orange'
+// })
 
-//on the wikipidia
-const h2 = document.querySelectorAll('h2')
-//for color change and padding 
-h2.forEach(function (h) {
-    h.style.color = 'black';
-    h.style.backgroundColor = '#93C572';
-    h.style.padding = '10px'
-})
+// //on the wikipidia
+// const h2 = document.querySelectorAll('h2')
+// //for color change and padding 
+// h2.forEach(function (h) {
+//     h.style.color = 'black';
+//     h.style.backgroundColor = '#93C572';
+//     h.style.padding = '10px'
+// })
 
-//for the innerText change in wikipidia
-h2.forEach(function (h) {
-    h.style.color = 'black';
-    h.style.backgroundColor = '#93C572';
-    h.style.padding = '10px';
-    h.innerText = 'Punitan salavan'
-})
-
+// //for the innerText change in wikipidia
+// h2.forEach(function (h) {
+//     h.style.color = 'black';
+//     h.style.backgroundColor = '#93C572';
+//     h.style.padding = '10px';
+//     h.innerText = 'Punitan salavan'
+// })
