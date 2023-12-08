b6443b09e66f01806fb77c0276bdfc2c90b79229
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-08 14:46:26 +0530

    Creating the js file and fire querySelectorAll

---
+++ DOM/One.js
@@ -0,0 +1,11 @@
+// document.getElementById("title")
+console.log("hello");
+document.querySelectorAll('li')
+const allLi = document.querySelectorAll('li')
+allLi.forEach(function (l) {
+    l.style.backgroundColor = 'purple'
+})
+
+//code generate the purple of All Li 
+
+
