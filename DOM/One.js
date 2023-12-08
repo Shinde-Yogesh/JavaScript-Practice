f0f890493e8341b0f624463a21cabfb250b2e9f0
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-08 15:16:09 +0530

    work on the wikipidia

--- DOM/One.js
+++ DOM/One.js
@@ -1,4 +1,6 @@
 // document.getElementById("title")
+
+//code generate the purple of All Li 
 console.log("hello");
 document.querySelectorAll('li')
 const allLi = document.querySelectorAll('li')
@@ -6,6 +8,27 @@ allLi.forEach(function (l) {
     l.style.backgroundColor = 'purple'
 })
 
-//code generate the purple of All Li 
+// get the all files of the class item
+//because class doesn't have loop so convert into the array
+Array.from(allLi)
+newArray.forEach(function (li){
+    li.style.color = 'orange'
+})
 
+//on the wikipidia
+const h2 = document.querySelectorAll('h2')
+//for color change and padding 
+h2.forEach(function (h) {
+    h.style.color = 'black';
+    h.style.backgroundColor = '#93C572';
+    h.style.padding = '10px'
+})
+
+//for the innerText change in wikipidia
+h2.forEach(function (h) {
+    h.style.color = 'black';
+    h.style.backgroundColor = '#93C572';
+    h.style.padding = '10px';
+    h.innerText = 'Punitan salavan'
+})
 
